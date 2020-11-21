dbc55f9041c2e288089b77cced222bbb72f1c393
Adam-Rapoport <adam.rapoport@chicagobooth.edu>
2020-11-21 12:26:28 +0000

    Working on building part

--- app/controllers/buildings_controller.rb
+++ app/controllers/buildings_controller.rb
@@ -1,6 +1,8 @@
 class BuildingsController < ApplicationController
   def index
-    matching_buildings = Building.all
+    @player = Player.where({ :id => session[:player_id] }).first
+    matching_buildings = @player.buildings
+    #matching_buildings = Building.where({ :builder_id => session[:player_id] })
 
     @list_of_buildings = matching_buildings.order({ :created_at => :desc })
 
@@ -20,13 +22,13 @@ class BuildingsController < ApplicationController
   def create
     the_building = Building.new
     the_building.building_type_id = params.fetch("query_building_type_id")
-    the_building.builder_id = params.fetch("query_builder_id")
+    the_building.builder_id = session[:player_id]
 
-    if the_building.valid?
+    if the_building.valid? # and have enough resources
       the_building.save
-      redirect_to("/buildings", { :notice => "Building created successfully." })
+      redirect_to("/buildings", { :notice => "A #{the_building.building_type.building_type_name} was built successfully!" })
     else
-      redirect_to("/buildings", { :notice => "Building failed to create successfully." })
+      redirect_to("/buildings", { :notice => "Not enough resources!" })
     end
   end
 
